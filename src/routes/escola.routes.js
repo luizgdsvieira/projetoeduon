c09c73e18072be96b0b895094ccfb95f30e73190
unknown <luizgdsieira@gmail.com>
2025-09-04 05:34:28 -0300

    novatualização20

--- src/routes/escola.routes.js
+++ src/routes/escola.routes.js
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 const router = Router();
 import { getSchool } from '../controllers/escola.controller.js';
-import auth from '../middleware/auth.middleware.js';
+import { authenticate, authorizeRoles } from '../middleware/auth.middleware.js';
 
 router.get('/', auth, getSchool);
 
