329bbd133e64689f3b7bb021f40413b275b42a99
unknown <luizgdsieira@gmail.com>
2025-09-04 05:12:00 -0300

    novatualização17

--- src/routes/aluno.routes.js
+++ src/routes/aluno.routes.js
@@ -1,7 +1,8 @@
 import { Router } from 'express';
 const router = Router();
 import { getAll, getById } from '../controllers/aluno.controller.js';
-import auth from '../middleware/auth.middleware.js';
+import { authenticate, authorizeRoles } from '../middleware/auth.middleware.js';
+
 
 router.get('/', auth, getAll);
 router.get('/:id', auth, getById);
