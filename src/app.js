11e2e37dac2f3fc42f84558ad30c807d1328c2fc
unknown <luizgdsieira@gmail.com>
2025-09-16 14:38:46 -0300

    atual-eduon-api-10

--- src/app.js
+++ src/app.js
@@ -17,4 +17,10 @@ app.use('/api/funcionarios', funcionarioRoutes);
 app.use('/api/escola', escolaRoutes);
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`API EDUON rodando na porta ${PORT}`));
+app.listen(PORT, () => { 
+    console.log(`API EDUON rodando na porta ${PORT}`)
+    console.log("🌍 Variáveis de ambiente:");
+    console.log("SUPABASE_URL:", process.env.SUPABASE_URL);
+    console.log("Database URL:", process.env.DATABASE_URL ? "definido ✅" : "não definido ❌");
+    console.log("JWT_SECRET:", process.env.JWT_SECRET ? "definido ✅" : "não definido ❌");
+});
