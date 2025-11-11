463d8bb25f4fcf44fca897e05776552c28cd5cf8
unknown <luizgdsieira@gmail.com>
2025-11-11 20:09:28 -0300

    att-eduon-api-46

--- src/routes/aluno.routes.js
+++ src/routes/aluno.routes.js
@@ -2,6 +2,8 @@ import { Router } from 'express';
 const router = Router();
 import { getAll, getById, create } from '../controllers/aluno.controller.js';
 import { authenticate, authorizeRoles } from '../middleware/auth.middleware.js';
+import { authMiddleware } from '../middleware/auth.middleware.js';
+
 
 // Rotas POST
 router.post('/', authenticate, authorizeRoles('admin'), create);
